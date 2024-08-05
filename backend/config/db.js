ba5273872344ea4007dce0c1f6df54780746d2ca
Joy Majumder <87913720+joymajumder-github@users.noreply.github.com>
2024-08-05 18:42:01 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB(){
     try{
-        await mongoose.connect('mongodb+srv://joymazumder027:joymazumder027@ecommerse.bof5dhd.mongodb.net/Ecommerse?retryWrites=true&w=majority&appName=Ecommerse'I)
+        await mongoose.connect('mongodb+srv://joymazumder027:joymazumder027@ecommerse.bof5dhd.mongodb.net/Ecommerse?retryWrites=true&w=majority&appName=Ecommerse')
     }catch(err){
         console.log(err)
     }
